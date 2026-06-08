ba91ebba0cb4208e21d739801b67051af3f47763
cfyzgblh <cU2roma@yandex.ru>
2026-06-08 21:01:49 +0300

    Add AddressRepository

---
+++ Bank.Infrastructure/Data/Repositories/Implementations/AddressRepository.cs
@@ -0,0 +1,56 @@
+﻿using Bank.Domain.Entities;
+using Bank.Infrastructure.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank.Infrastructure.Data.Repositories.Implementations
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private readonly BankDbContext _context;
+
+        //Конструктор
+        public AddressRepository(BankDbContext context)
+            => _context = context;
+
+        //Добавление адреса 
+        public async Task<Address> AddAddress(DateTime registrationDate, string area, string city, string street, string building, string appartment, string code)
+        {
+            var address = new Address
+            {
+                RegistrationDate = registrationDate,
+                Area = area,
+                City = city,
+                Street = street,
+                Building = building,
+                Apartment = appartment,
+                Code = code
+            };
+
+            await _context.Addresses.AddAsync(address);
+            await _context.SaveChangesAsync();
+            return address;
+        }
+
+        //Получение всех адресов
+        public async Task<List<Address>> GetAllAddreses()
+            => await _context.Addresses.ToListAsync();
+
+        //Получение адреса по id пользователя
+        public async Task<Address> GetAddresByUserId(int userId)
+            => await _context.Addresses.SingleOrDefaultAsync(a => a.UserId == userId);
+
+        //Получение адреса по id
+        public async Task<Address> GetAddressById(int id)
+            => await _context.Addresses.SingleOrDefaultAsync(a => a.Id == id);
+
+        //Удаление адреса
+        public async Task Delete(Address addressToDelete)
+        {
+            _context.Addresses.Remove(addressToDelete);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Bank.Infrastructure/Data/Repositories/Interfaces/IAddressRepository.cs
@@ -0,0 +1,18 @@
+﻿using Bank.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Bank.Infrastructure.Data.Repositories.Interfaces
+{
+    public interface IAddressRepository
+    {
+        public Task<List<Address>> GetAllAddreses();
+        public Task<Address> AddAddress(DateTime registrationDate, string area, string city, string street, string building, string appartment, string code);
+        public Task<Address> GetAddressById(int id);
+        public Task<Address> GetAddresByUserId(int userId);
+        public Task Delete(Address addressToDelete);
+
+    }
+}
